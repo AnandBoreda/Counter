8288298e3ab09c522c4324a908a696871a09ce94
AnandBoreda <zz.anand2016@gmail.com>
2020-07-20 12:26:26 +0530

    replaced class with sfc and object destructured for props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+import React from "react";
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar-light bg-light">
+      <a
+        className="navbar-brand"
+        href="https://github.com/anandboreda/counter.git"
+      >
+        &nbsp; Total items &nbsp;
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
